9c1ec3f93fcae847862e63b372726e5d033576b6
WayneZhouChina <cumtxhzyy@gmail.com>
2016-10-09 10:36:22 +0800

    fix unit test bug

--- gitlab2gs_test.go
+++ gitlab2gs_test.go
@@ -4,13 +4,13 @@ import (
 	"testing"
 )
 
-func TestloadConfig(t *testing.T) {
+func TestLoadConfig(t *testing.T) {
 	config := loadConfig("./config.json.sample")
 	if config.GitlabUser != "username" {
 		t.Errorf("load config file error")
 	}
 
 	if config.GitlabPassword != "password" {
-		t.Errorf("load config file error")
+		t.Error("load config file error")
 	}
 }
